e6380cdcbcc81d13bb724da00bbfea63614444f1
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-27 13:12:27 +0200

    first

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# twenty
-Deploy Twenty with CI/CD on Elestio
+# Twenty CI/CD pipeline
+
+<a href="https://dash.elest.io/deploy?source=cicd&social=dockerCompose&url=https://github.com/elestio-examples/twenty"><img src="deploy-on-elestio.png" alt="Deploy on Elest.io" width="180px" /></a>
+
+Deploy Twenty server with CI/CD on Elestio
+
+<img src="twenty.png" style='width: 100%;'/>
+<br/>
+<br/>
+
+# Once deployed ...
+
+You can open Twenty UI here:
+
+    URL: https://[CI_CD_DOMAIN]
+    login: "[ADMIN_EMAIL]"
+    password: "[ADMIN_PASSWORD]"
+
+You can open pgAdmin here:
+
+    URL: https://[CI_CD_DOMAIN]:63405
+    login: "[ADMIN_EMAIL]"
+    password: "[ADMIN_PASSWORD]"
---
+++ scripts/preInstall.sh
@@ -0,0 +1,32 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+ACCESS_TOKEN_SECRET=$(openssl rand -base64 32)
+LOGIN_TOKEN_SECRET=$(openssl rand -base64 32)
+REFRESH_TOKEN_SECRET=$(openssl rand -base64 32)
+FILE_TOKEN_SECRET=$(openssl rand -base64 32)
+
+cat << EOT >> ./scripts/config.sh
+
+ACCESS_TOKEN_SECRET=${ACCESS_TOKEN_SECRET}
+LOGIN_TOKEN_SECRET=${LOGIN_TOKEN_SECRET}
+REFRESH_TOKEN_SECRET=${REFRESH_TOKEN_SECRET}
+FILE_TOKEN_SECRET=${FILE_TOKEN_SECRET}
+EOT
+
+cat <<EOT > ./servers.json
+{
+    "Servers": {
+        "1": {
+            "Name": "local",
+            "Group": "Servers",
+            "Host": "172.17.0.1",
+            "Port": 51175,
+            "MaintenanceDB": "postgres",
+            "SSLMode": "prefer",
+            "Username": "postgres",
+            "PassFile": "/pgpass"
+        }
+    }
+}
+EOT
