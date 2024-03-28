93c1a8c57e63681b4414ced2dda7eea84bc9e9be
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-28 16:15:14 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,9 +5,8 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 60s;
 
-target=$(docker-compose port server 3000)
 
-curl http://target/graphql \
+curl ${SERVER_URL}/graphql \
   -H 'accept: */*' \
   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
   -H 'authorization;' \
@@ -15,7 +14,7 @@ curl http://target/graphql \
   -H 'content-type: application/json' \
   -H 'pragma: no-cache' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
-  --data-raw $'{"operationName":"SignUp","variables":{"email":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'"},"query":"mutation SignUp($email: String\u0021, $password: String\u0021, $workspaceInviteHash: String) {\\n  signUp(\\n    email: $email\\n    password: $password\\n    workspaceInviteHash: $workspaceInviteHash\\n  ) {\\n    loginToken {\\n      ...AuthTokenFragment\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment AuthTokenFragment on AuthToken {\\n  token\\n  expiresAt\\n  __typename\\n}"}'
+  --data-raw "{\"operationName\":\"SignUp\",\"variables\":{\"email\":\"${ADMIN_EMAIL}\",\"password\":\"${ADMIN_PASSWORD}\"},\"query\":\"mutation SignUp(\$email: String!, \$password: String!, \$workspaceInviteHash: String) {\\n  signUp(\\n    email: \$email\\n    password: \$password\\n    workspaceInviteHash: \$workspaceInviteHash\\n  ) {\\n    loginToken {\\n      ...AuthTokenFragment\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment AuthTokenFragment on AuthToken {\\n  token\\n  expiresAt\\n  __typename\\n}\"}"
 
   docker-compose down;
 
